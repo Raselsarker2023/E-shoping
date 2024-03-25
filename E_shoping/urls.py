af69e33b1e8a6a1f39a95dacdecda3215fece321
Your Name <your.email@example.com>
2024-03-25 10:59:01 +0600

    updated successfully

--- E_shoping/urls.py
+++ E_shoping/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 from rest_framework.routers import DefaultRouter
 from . views import UserViewSet
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
@@ -16,5 +19,5 @@ urlpatterns = [
     path('order/', include('order.urls')),
     path('card/', include('card.urls')),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
