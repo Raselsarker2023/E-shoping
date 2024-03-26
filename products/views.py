d1a5c6a5a9955f68ec15f711f3312b40882091b2
Your Name <your.email@example.com>
2024-03-26 15:57:00 +0600

    update products search

--- products/views.py
+++ products/views.py
@@ -5,18 +5,17 @@ from rest_framework import status
 from . import models, serializers
 from rest_framework import filters
 from django.http import Http404
+from rest_framework.exceptions import ValidationError
+import uuid
 
 
 class ProductModelAPIView(viewsets.ModelViewSet):
     queryset = models.ProductModel.objects.all()
     serializer_class = serializers.ProductModelSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['name', 'category_name', 'description', 'rating']
+    search_fields = ['name', 'category__name', 'description', 'rating']
 
 
-    
-from rest_framework.exceptions import ValidationError
-import uuid
 
 class ReviewAPIView(viewsets.ModelViewSet):
     queryset = models.Review.objects.all()
@@ -24,22 +23,23 @@ class ReviewAPIView(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', 'email', 'body', 'rating']
     
-    def create(self, request, *args, **kwargs):
-        product_id = kwargs["pk"]
-        if not self.is_valid_uuid(product_id):
-            raise ValidationError({"product_id": ["Must be a valid UUID."]})
-        request.data["reviewer"] = product_id
-        return super().create(request, *args, **kwargs)
     
-    def is_valid_uuid(self, uuid_to_test, version=4):
-        try:
-            uuid_obj = uuid.UUID(uuid_to_test, version=version)
-        except ValueError:
-            return False
-        return str(uuid_obj) == uuid_to_test
+    # def create(self, request, *args, **kwargs):
+    #     product_id = kwargs["pk"]
+    #     if not self.is_valid_uuid(product_id):
+    #         raise ValidationError({"product_id": ["Must be a valid UUID."]})
+    #     request.data["reviewer"] = product_id
+    #     return super().create(request, *args, **kwargs)
     
-    def get_queryset(self):
-        return models.Review.objects.filter(reviewer_id=self.kwargs["pk"])
+    # def is_valid_uuid(self, uuid_to_test, version=4):
+    #     try:
+    #         uuid_obj = uuid.UUID(uuid_to_test, version=version)
+    #     except ValueError:
+    #         return False
+    #     return str(uuid_obj) == uuid_to_test
+    
+    # def get_queryset(self):
+    #     return models.Review.objects.filter(reviewer_id=self.kwargs["pk"])
 
 
 
