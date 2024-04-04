f8fb50b34d28c6dec7d1b8d515693be92bf3f4c8
Your Name <your.email@example.com>
2024-04-04 11:45:26 +0600

    updated cartItem

--- card/models.py
+++ card/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from order.models import OrderList
 from products.models import ProductModel
 
 # Create your models here.
@@ -21,6 +20,9 @@ class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(ProductModel, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.PositiveIntegerField(default=1, null=True, blank=True)
+    produce_name = models.CharField(max_length=15, null=True, blank=True)
+    product_image_url = models.URLField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name} in Cart"
